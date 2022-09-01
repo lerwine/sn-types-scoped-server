9fc77c8b1ee8e0d3e7ef9a5be8de794c63afca0b
Leonard <lenny@erwinefamily.net>
2022-09-01 02:39:09 -0400

    Added sn_schedule_pages

---
+++ sn_schedule_pages.d.ts
@@ -0,0 +1,14 @@
+
+export namespace sn_schedule_pages {
+    /**
+     * Contains utilities for schedules and schedules pages calendar
+     * @export
+     * @class SchedulePageUtilSNC
+     */
+    export class SchedulePageUtilSNC {
+        // TODO: Add members for sn_schedule_pages.SchedulePageUtilSNC
+        // https://dev109722.service-now.com/nav_to.do?uri=sys_script_include.do?sys_id=7a9cb8150b033200ff6707a7b6673a6c
+
+        type: "SchedulePageUtilSNC"
+    }
+}
