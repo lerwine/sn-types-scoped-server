a12f903520826b1b7049d20151bac459d7d3d688
Leonard <lenny@erwinefamily.net>
2022-09-05 01:30:25 -0400

    Fixed code format

--- sys_script.d.ts
+++ sys_script.d.ts
@@ -4,9 +4,9 @@
  * @class JSON
  * @see {@link https://docs.servicenow.com/bundle/rome-application-development/page/app-store/dev_portal/API_reference/JSON/concept/c_JSONAPI.html}
  */
-    export class JSON {
+export class JSON {
     // TODO: Add members for global.JSON
     // https://dev109722.service-now.com/nav_to.do?uri=sys_script_include.do?sys_id=d2426c9ec0a8016501958bf2ac79c775
 
-    type: "JSON"
+    type: "JSON";
 }
